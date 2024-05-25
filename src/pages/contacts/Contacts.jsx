4e8590a3abe9798bae190bb95cb229976eb63693
Irfankhan20 <sipathan96@gmail.com>
2024-05-25 10:38:39 +0600

    cards decorate sort by depend on price & rating

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
+import MailBox from "./MailBox";
 
 const Contacts = () => {
   const controls = useAnimation();
@@ -16,16 +17,18 @@ const Contacts = () => {
         alt=""
       />
       {/* support card section  */}
-      <h2 className="text-2xl font-bold text-center pb-10">Support Session</h2>
+      <h2 className="text-3xl  font-medium text-center pb-10">
+        Support Session
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-20">
         {/* card 1 */}
         <motion.div
           animate={controls}
           whileHover={{ scale: 1.3 }}
           transition={{ duration: 0.3 }}
-          className="bg-[#e0f2f2] rounded-xl shadow-xl "
+          className="bg-[#FFFFFF] rounded-xl shadow-xl "
         >
-          <div className="bg-[#e0f2f2] p-10 rounded-xl shadow-xl">
+          <div className="bg-[#FFFFFF] p-10 rounded-xl shadow-xl">
             <h2 className="font-bold underline">Sales</h2>
             <p className="my-6">
               Whether you're new to GREEMIND or a current customer, get in touch
@@ -42,9 +45,9 @@ const Contacts = () => {
           animate={controls}
           whileHover={{ scale: 1.3 }}
           transition={{ duration: 0.3 }}
-          className="bg-[#e0f2f2] rounded-xl shadow-xl "
+          className="bg-[#FFFFFF] rounded-xl shadow-xl "
         >
-          <div className="bg-[#e0f2f2] p-10 rounded-xl shadow-xl">
+          <div className="bg-[#FFFFFF] p-10 rounded-xl shadow-xl">
             <h2 className="font-bold underline">Support</h2>
             <p className="my-6">
               Access resources to help our customers learn how to better utilize
@@ -61,9 +64,9 @@ const Contacts = () => {
           animate={controls}
           whileHover={{ scale: 1.3 }}
           transition={{ duration: 0.3 }}
-          className="bg-[#e0f2f2] rounded-xl shadow-xl "
+          className="bg-[#FFFFFF] rounded-xl shadow-xl "
         >
-          <div className="bg-[#e0f2f2] p-10 rounded-xl shadow-xl">
+          <div className="bg-[#FFFFFF] p-10 rounded-xl shadow-xl">
             <h2 className="font-bold underline">Partners</h2>
             <p className="my-6">
               Join our mission to show the world that social is more than a
@@ -74,6 +77,11 @@ const Contacts = () => {
             </button>
           </div>
         </motion.div>
+      </div>
+
+      {/* poster section  */}
+      <div className="mb-20">
+        <MailBox></MailBox>
       </div>
     </div>
   );
